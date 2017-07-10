13347538847503649a9a7e900a52e06482599d4b
TaWReOn <deniss.mosin@gmail.com>
2017-07-10 16:36:22 +0300

    playground

--- src/app/domain/playground/playground.controller.js
+++ src/app/domain/playground/playground.controller.js
@@ -5,26 +5,37 @@
     .module('webPage')
     .controller('PlayGroundController', PlayGroundController);
 
-  function PlayGroundController($scope, Schema, saControllerHelper) {
+  function PlayGroundController($scope, Schema, saControllerHelper, $state) {
 
     const vm = saControllerHelper.setup(this, $scope);
 
     const {Employee, Person, ServicePoint} = Schema.models();
 
-    vm.use({});
+    vm.use({
+      goToDetailed
+    });
+
+
+    function goToDetailed(point) {
+      $state.go('.detailed', {id: point.id});
+
+      ServicePoint.find(point.id).then((a, b) => {
+        console.error(a, b);
+      });
+
+    }
 
     Person.findAll();
 
     ServicePoint.findAll()
       .then(data => {
+        console.log('ServicePoint fired');
         vm.data = data;
-        _.each(data, (a, b) => {
-          console.log(a, b);
-        });
       });
 
     Employee.find('dc2a0b00-5bef-11e7-8000-6c40089c5fc0')
       .then(data => console.log(data));
 
   }
+
 })();
