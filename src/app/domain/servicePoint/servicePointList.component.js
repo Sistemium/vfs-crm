efe971e04a9331d32525edd32d4a6b8f5985dee9
TaWReOn <deniss.mosin@gmail.com>
2017-07-12 19:02:53 +0300

    servicePointList

--- src/app/domain/servicePoint/servicePointList.component.js
+++ src/app/domain/servicePoint/servicePointList.component.js
@@ -2,53 +2,14 @@
 
   module.component('servicePointList', {
 
+    bindings: {
+      servicePoints: '=',
+      servicePointClickFn: '='
+    },
+
     templateUrl: 'app/domain/servicePoint/servicePointList.html',
-    controller: servicePointListController,
     controllerAs: 'vm'
 
   });
-
-
-  function servicePointListController($scope, Schema, saControllerHelper, $state) {
-
-    const vm = saControllerHelper.setup(this, $scope);
-
-    const {Employee, Person, ServicePoint} = Schema.models();
-
-    vm.use({
-      servicePointClick
-    });
-
-    vm.rebindAll(ServicePoint, {}, 'vm.data');
-
-    refresh();
-
-    /*
-     Functions
-     */
-
-    function servicePointClick(servicePoint) {
-
-      $state.go('.detailed', {servicePointId: servicePoint.id});
-
-      // ServicePoint.find(servicePoint.id);
-
-    }
-
-    function refresh() {
-
-      let busy = [
-        Person.findAll(),
-        Employee.findAll(),
-        ServicePoint.findAll()
-      ];
-
-      vm.setBusy(busy);
-
-    }
-
-
-  }
-
 
 })(angular.module('webPage'));
