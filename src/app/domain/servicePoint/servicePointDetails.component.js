b4e3f98ac4d8690450b858f9c266e81fb20c3198
TaWReOn <deniss.mosin@gmail.com>
2017-07-17 19:28:11 +0300

    service point component

--- src/app/domain/servicePoint/servicePointDetails.component.js
+++ src/app/domain/servicePoint/servicePointDetails.component.js
@@ -13,14 +13,18 @@
 
     const vm = saControllerHelper.setup(this, $scope);
 
-    const {ServicePoint, FilterSystem, Brand} = Schema.models();
+    const {ServicePoint, FilterSystem, Brand, Person} = Schema.models();
 
-    vm.use({});
+    vm.use({
+      onItemHover,
+      editItem
+    });
 
     refresh();
 
     FilterSystem.findAll();
     Brand.findAll();
+    Person.findAll();
 
     /*
      Functions
@@ -30,19 +34,50 @@
 
       let id = $state.params.servicePointId;
 
+      vm.rebindOne(ServicePoint, id, 'vm.servicePoint');
+
       let busy = [
-        ServicePoint.find(id),
-        ServicePoint.findAllWithRelations({id}, {bypassCache: true})(['ServiceItem'])
+        ServicePoint.findAllWithRelations({id}, {bypassCache: true})(['ServiceItem', 'ServicePointContact'])
+          .then(loadServicePointRelations)
       ];
 
-      vm.setBusy(busy)
-        .then(res => {
-          vm.servicePoint = res[0];
-          console.warn(res[0]);
-        });
+      vm.setBusy(busy);
+    }
+
+    function loadServicePointRelations(data) {
+
+      let servicePoint = _.first(data);
+
+      if (!servicePoint) return;
+
+      _.each(servicePoint.servingItems, serviceItem => {
+        serviceItem.DSLoadRelations()
+          .then(() => {
+            _.each(serviceItem.serviceContractItems, serviceContractItem => {
+              serviceContractItem.DSLoadRelations();
+            });
+          });
+      });
+
+      console.warn(servicePoint);
 
     }
 
+    function onItemHover(item) {
+
+      if (item) {
+        vm.currHoverId = item.id;
+        vm.showIcon = true;
+      } else {
+        vm.currHoverId = '';
+        vm.showIcon = false;
+      }
+
+    }
+
+    function editItem(item) {
+
+    }
 
   }
 
