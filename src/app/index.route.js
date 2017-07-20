8bbc38539d7b01f6bdf4bc14f12754e96a280f27
TaWReOn <deniss.mosin@gmail.com>
2017-07-20 19:01:29 +0300

    routerDecorator

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,22 +1,47 @@
-(function() {
+(function () {
   'use strict';
 
   angular
-    .module('webPage')
-    .config(routerConfig);
+  .module('webPage')
+  .config(routerConfig)
+  .run(routerDecorator);
 
   /** @ngInject */
   function routerConfig($stateProvider, $urlRouterProvider) {
     $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainController',
-        controllerAs: 'vm'
-      })
+    .state('home', {
+      url: '/',
+      templateUrl: 'app/main/main.html',
+      controller: 'MainController',
+      controllerAs: 'vm'
+    })
     ;
 
     $urlRouterProvider.otherwise('/');
+  }
+
+  function routerDecorator($rootScope, localStorageService, $state) {
+
+    $rootScope.$on('$stateChangeStart', (event, toState, toParams) => {
+
+      if (!toState.defaultChild) {
+        return;
+      }
+
+      let mode = localStorageService.get(`${toState.name}.mode`) || toState.defaultChild;
+
+      //if (toParams.item) {
+      //  mode += '.item';
+      //  toParams = {
+      //    id: toParams.item
+      //  }
+      //}
+      event.preventDefault();
+
+      return $state.go(`${toState.name}.${mode}`, toParams, {inherit: false});
+
+    });
+
   }
 
 })();
