9f526ac03796cb3cbadb70074a325746ac614dae
TaWReOn <deniss.mosin@gmail.com>
2017-07-21 19:07:00 +0300

    navbar modal open on + button click

--- src/app/domain/components/navigationBar/navigationBar.component.js
+++ src/app/domain/components/navigationBar/navigationBar.component.js
@@ -14,19 +14,39 @@
 
   });
 
-  function navigationBarController($scope, saControllerHelper, $state) {
+  function navigationBarController($scope, saControllerHelper, $state, $uibModal) {
 
     const vm = saControllerHelper.setup(this, $scope);
 
     vm.use({
       goToTable,
       goToTiles,
+      addItem,
       active: 'tiles'
     });
 
     /*
      Functions
      */
+
+    function addItem() {
+      openEditItemModal()
+    }
+
+    function openEditItemModal(point) {
+      $uibModal.open({
+        animation: true,
+        ariaLabelledBy: 'modal-title-bottom',
+        ariaDescribedBy: 'modal-body-bottom',
+        templateUrl: 'app/domain/components/navigationBar/newServicePoint.html',
+        size: 'lg',
+        controller: function () {
+          let vm = this;
+          vm.point = point;
+        },
+        controllerAs: 'vm'
+      });
+    }
 
     function goToTiles() {
       $state.go(vm.stateName + '.tiles');
