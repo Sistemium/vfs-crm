df035242ace94710886657230f7b1431392c3b9b
TaWReOn <deniss.mosin@gmail.com>
2017-07-24 19:27:47 +0300

    employeeTiles.component.js

---
+++ src/app/domain/employee/employeeTiles.component.js
@@ -0,0 +1,35 @@
+(function (module) {
+
+  module.component('employeeTiles', {
+
+    bindings: {
+      employees: '='
+    },
+
+    templateUrl: 'app/domain/employee/employeeTiles.html',
+    controller: employeeTilesController,
+    controllerAs: 'vm'
+
+  });
+
+  function employeeTilesController($scope, saControllerHelper) {
+
+    const vm = saControllerHelper.setup(this, $scope);
+
+    vm.use({
+      editItem,
+      isOpenedEditPopover: [],
+      currentItem: null
+    });
+
+    /*
+     Functions
+     */
+
+    function editItem(item) {
+      vm.currItem = item.id;
+    }
+
+  }
+
+})(angular.module('webPage'));
