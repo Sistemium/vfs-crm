ab4bdae4c6839f0fe7266f3e0b417cfae62d002f
TaWReOn <deniss.mosin@gmail.com>
2017-07-25 19:20:51 +0300

    $rootScope.$broadcast('authorized')

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -3,33 +3,33 @@
 (function () {
 
   angular
-    .module('webPage')
-    .run(run)
-    .service('DEBUG', debugService)
-    .config(localStorageServiceProvider => {
-      localStorageServiceProvider.setPrefix('vfs');
-    })
-    .run(amMoment => {
-      amMoment.changeLocale('lt');
-    })
-    .config($locationProvider => {
-      $locationProvider.hashPrefix('');
-    })
-    .config($compileProvider => {
-      $compileProvider.preAssignBindingsEnabled(true);
-    })
+  .module('webPage')
+  .run(run)
+  .service('DEBUG', debugService)
+  .config(localStorageServiceProvider => {
+    localStorageServiceProvider.setPrefix('vfs');
+  })
+  .run(amMoment => {
+    amMoment.changeLocale('lt');
+  })
+  .config($locationProvider => {
+    $locationProvider.hashPrefix('');
+  })
+  .config($compileProvider => {
+    $compileProvider.preAssignBindingsEnabled(true);
+  })
   ;
 
   function debugService(saDebug) {
     return saDebug.log('stg:log');
   }
 
-  function run(Sockets, InitService, saApp, IOS, localStorageService, DEBUG) {
+  function run(Sockets, InitService, $rootScope, IOS, localStorageService, DEBUG) {
 
     InitService
-      .then(Sockets.init)
-      // .then(saApp.init)
-      .catch(error => localStorageService.set('error', angular.toJson(error)));
+    .then(Sockets.init)
+    // .then(saApp.init)
+    .catch(error => localStorageService.set('error', angular.toJson(error)));
 
     // Auth.init(IOS.isIos() ? IOS.init() : phaService).then();
 
@@ -39,14 +39,14 @@
 
       let appConfig =
         // InitService.localDevMode ? {} :
-          {
-            url: {
-              socket: 'https://socket2.sistemium.com'
-            }
+        {
+          url: {
+            socket: 'https://socket2.sistemium.com'
           }
+        }
       ;
 
-      let org ='vfs';
+      let org = 'vfs';
 
       if (!IOS.isIos()) {
         angular.extend(appConfig, {
@@ -54,7 +54,6 @@
           org
         });
       }
-
 
       InitService.then(() => Sockets.on('connect', sockAuth));
       InitService.init(appConfig);
@@ -65,6 +64,7 @@
 
         Sockets.emit('authorization', {accessToken: accessToken}, function (ack) {
           DEBUG('Socket authorization:', ack);
+          $rootScope.$broadcast('authorized')
         });
 
       }
