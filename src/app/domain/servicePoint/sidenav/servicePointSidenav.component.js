aaa58f2a0072fdad3563cd1416bb82a9b2445fb7
TaWReOn <deniss.mosin@gmail.com>
2017-07-25 18:04:46 +0300

    rm filter, orderBy from view

--- src/app/domain/servicePoint/sidenav/servicePointSidenav.component.js
+++ src/app/domain/servicePoint/sidenav/servicePointSidenav.component.js
@@ -1,6 +1,7 @@
 (function (module) {
 
   module.component('servicePointSidenav', {
+
     bindings: {
       servicePoints: '=',
       currentServicePointId: '=',
@@ -8,20 +9,35 @@
       currentIdx: '=',
       promise: '='
     },
+
     templateUrl: 'app/domain/servicePoint/sidenav/servicePointSidenav.html',
     controller: servicePointSidenav,
     controllerAs: 'vm'
 
   });
 
-  function servicePointSidenav(saControllerHelper, $scope, $q) {
+  function servicePointSidenav(saControllerHelper, $scope, $q, $filter, Schema) {
 
     const vm = saControllerHelper.setup(this, $scope);
     const servicePointHeight = 53;
 
+    const {ServicePoint} = Schema.models();
+
+    vm.watchScope('vm.searchText', onSearch);
+    vm.rebindAll(ServicePoint, {}, 'vm.data', onSearch);
+
     vm.use({
       restoreScrollPosition
     });
+
+    function onSearch() {
+
+      let {searchText} = vm;
+
+      vm.servicePoints = searchText ? $filter('filter')(vm.data, searchText) : vm.data;
+      vm.servicePoints = $filter('orderBy')(vm.servicePoints, 'name');
+
+    }
 
     function restoreScrollPosition() {
 
@@ -30,6 +46,8 @@
         if (vm.servicePoints.length <= 1) {
           vm.servicePoints = res[1];
         }
+
+        vm.servicePoints = $filter('orderBy')(vm.servicePoints, 'name');
 
         let servicePoints = _.orderBy(vm.servicePoints, ['name'], ['asc']);
         let idx = _.findIndex(servicePoints, {id: vm.currentServicePointId});
