17eb2c9730fa16da09e4153653a5d39b0763b5f0
TaWReOn <deniss.mosin@gmail.com>
2017-07-25 18:05:46 +0300

    editing service

---
+++ src/app/domain/components/editing.service.js
@@ -0,0 +1,67 @@
+'use strict';
+
+(function () {
+
+  angular.module('webPage')
+  .service('Editing', Editing);
+
+  function Editing($uibModal) {
+
+    return {editModal};
+
+    function editModal(componentName, title) {
+      return item => openEditModal(item, componentName, title);
+    }
+
+    function openEditModal(item, componentName, title) {
+
+      let modal = $uibModal.open({
+
+        animation: true,
+        template: `<div class="modal-header">{{title}}</div>` +
+        `<${componentName} service-point="item" save-fn="saveFn"></${componentName}>` +
+        `<div class="modal-footer">
+          <button class="btn btn-success" ng-click="saveClick()">Išsaugoti</button>
+          <button class="btn btn-danger" ng-click="cancelClick()">Atšaukti</button>
+         </div>`,
+        size: 'lg',
+
+        controller
+
+      });
+
+      modal.result.catch(() => {
+        item.DSRevert();
+      });
+
+      return modal.result;
+
+      function controller($scope) {
+
+        _.assign($scope, {
+          item,
+          title,
+          saveClick,
+          cancelClick
+        });
+
+        function saveClick() {
+          if ($scope.saveFn) {
+            $scope.saveFn()
+              .then(modal.close);
+          } else if (_.isFunction(item.DSCreate)) {
+            item.DSCreate(item)
+              .then(modal.close);
+          }
+        }
+
+        function cancelClick() {
+          modal.dismiss();
+        }
+      }
+
+    }
+
+  }
+
+})();
