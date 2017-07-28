77a9c145ae3c01441c6f9b898b8198d47eb02792
TaWReOn <deniss.mosin@gmail.com>
2017-07-28 19:33:14 +0300

    transformToComponent directive

---
+++ src/app/domain/directives/transformToComponent.directive.js
@@ -0,0 +1,43 @@
+'use strict';
+
+(function () {
+
+  function transformToComponentDirective($compile) {
+
+    return {
+
+      restrict: 'E',
+      scope: {
+        componentName: '@',
+        instance: '='
+      },
+      controller: transformToComponentController,
+      controllerAs: 'vm',
+      bindToController: true,
+
+      link: function (scope, element, attrs) {
+
+        let {componentName} = attrs;
+        let itemName = _.last(componentName.match(/edit-(.*)/));
+
+        var template = angular.element(`<${componentName} ${itemName}="vm.instance"></${componentName}>`);
+        element.append(template);
+        $compile(template)(scope);
+
+      }
+
+    };
+  }
+
+  angular.module('webPage')
+  .directive('transformToComponent', transformToComponentDirective);
+
+  function transformToComponentController() {
+
+    let vm = this;
+
+    _.assign(vm, {});
+
+  }
+
+})();
