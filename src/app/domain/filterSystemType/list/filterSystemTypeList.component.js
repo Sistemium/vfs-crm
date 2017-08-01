e91f4f718cf14ab4c5dd4dec3ed1ab5cde29493c
TaWReOn <deniss.mosin@gmail.com>
2017-08-01 11:44:03 +0300

    filterSystemType

--- src/app/domain/filterSystemType/list/filterSystemTypeList.component.js
+++ src/app/domain/filterSystemType/list/filterSystemTypeList.component.js
@@ -1,60 +1,18 @@
 (function (module) {
 
-  module.component('filterSystemTypeMaster', {
+  module.component('filterSystemTypeList', {
 
-    templateUrl: 'app/domain/filterSystemType/filterSystemTypeMaster.html',
-    controller: filterSystemTypeMasterController,
+    bindings: {
+      filterSystemTypes: '='
+    },
+
+    templateUrl: 'app/domain/filterSystemType/list/filterSystemTypeList.html',
+    controller: filterSystemTypeListController,
     controllerAs: 'vm'
 
   });
 
-  function filterSystemTypeMasterController($scope, Schema, saControllerHelper) {
-
-    const vm = saControllerHelper.setup(this, $scope);
-
-    const {FilterSystemType} = Schema.models();
-
-    vm.use({
-      $onInit
-    });
-
-    vm.rebindAll(FilterSystemType, {orderBy: ['name']}, 'vm.filterSystemTypes', onSearch);
-    vm.watchScope('vm.searchText', onSearch);
-
-    /*
-     Functions
-     */
-
-    function onSearch() {
-
-      let {searchText} = vm;
-      vm.filterSystemTypesFiltered = filterFilterSystemTypes(vm.filterSystemTypes, searchText);
-
-    }
-
-    function filterFilterSystemTypes(data, text) {
-
-      if (!text) return data;
-
-      let re = new RegExp(_.escapeRegExp(text), 'i');
-
-      return _.filter(data, item => re.test(item.name));
-
-    }
-
-    function $onInit() {
-      refresh();
-    }
-
-    function refresh() {
-
-      let busy = [
-        FilterSystemType.findAll({}, {bypassCache: true})
-      ];
-
-      vm.setBusy(busy);
-
-    }
+  function filterSystemTypeListController() {
   }
 
 })(angular.module('webPage'));
