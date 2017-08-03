c9f5e941a1f905c3ecbe8ca5469001e833c45af2
TaWReOn <deniss.mosin@gmail.com>
2017-08-03 17:46:32 +0300

    Entities routes moved to sep. file

--- src/app/routes/filterSystem.routes.js
+++ src/app/routes/filterSystem.routes.js
@@ -18,40 +18,7 @@
           }
 
         })
-        .state({
-
-          name: 'brand',
-          url: '/brand',
-          template: '<brand-master></brand-master>',
-
-          data: {
-            title: 'Prekių Ženklai'
-          }
-
-        })
-        .state({
-
-          name: 'filterSystemType',
-          url: '/filterSystemType',
-          template: '<filter-system-type-master></filter-system-type-master>',
-
-          data: {
-            title: 'Filtravimo Sistemų Tipai'
-          }
-
-        })
-        .state({
-
-          name: 'Site',
-          url: '/site',
-          template: '<site-master></site-master>',
-
-          data: {
-           title: 'Padaliniai'
-          }
-
-        });
-
+        
     });
 
 })();
