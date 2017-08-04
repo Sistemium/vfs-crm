01b653bf414c6287cea5b93da264d5468c20a682
TaWReOn <deniss.mosin@gmail.com>
2017-08-04 14:02:24 +0300

    openModal func

--- src/app/domain/brand/list/brandList.component.js
+++ src/app/domain/brand/list/brandList.component.js
@@ -3,14 +3,21 @@
   module.component('brandList', {
 
     bindings: {
-
       brands: '='
-
     },
 
     templateUrl: 'app/domain/brand/list/brandList.html',
+    controller: brandListController,
     controllerAs: 'vm'
 
   });
+
+  function brandListController(saControllerHelper, $scope, Editing) {
+    const vm = saControllerHelper.setup(this, $scope);
+
+    vm.use({
+      openModal: Editing.editModal('edit-brand', 'Prekės Ženklo Redagavimas'),
+    });
+  }
 
 })(angular.module('webPage'));
