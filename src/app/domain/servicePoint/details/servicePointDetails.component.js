20e03744415c3ca4316e7ab9a0ea604b177d5454
TaWReOn <deniss.mosin@gmail.com>
2017-08-16 19:19:53 +0300

    add-delete service point images

--- src/app/domain/servicePoint/details/servicePointDetails.component.js
+++ src/app/domain/servicePoint/details/servicePointDetails.component.js
@@ -8,24 +8,34 @@
 
   });
 
-  function servicePointDetailsController($scope, Schema, saControllerHelper, $state, Editing) {
+  function servicePointDetailsController($scope, Schema, saControllerHelper, $state, Editing, PictureHelper, GalleryHelper, $document, $timeout) {
 
-    const vm = saControllerHelper.setup(this, $scope);
+    const vm = saControllerHelper.setup(this, $scope)
+      .use(GalleryHelper);
 
-    const {ServicePoint, FilterSystem, Brand, Person, ServiceItem, ServicePointContact} = Schema.models();
+    const {ServicePoint, FilterSystem, Brand, Person, ServiceItem, ServicePointContact, Picture} = Schema.models();
 
     vm.use({
 
       isOpenEditPopover: {},
+      tileBusy: {},
+      progress: {},
+      uploadingPicture: false,
 
       editItemClick,
       addItemClick,
       contactClick,
       addContractPerson,
       phoneTo,
+      onChangeFile,
+      pictureUpload,
+      photoClick,
+      testScroll,
+      $onInit,
 
+      editPhoto: Editing.editModal('edit-picture', 'Fotografijos Redagavimas'),
       editServicePointClick: Editing.editModal('edit-service-point', 'Aptarnavimo Taško Redagavimas'),
-      editContractPerson: Editing.editModal('edit-service-point-contract', 'Redaguoti Naudotoją'),
+      editContractPerson: Editing.editModal('edit-service-point-contract', 'Redaguoti Naudotoją')
 
     });
 
@@ -38,6 +48,85 @@
     /*
      Functions
      */
+
+    function $onInit() {
+      let item = document.getElementsByClassName('more-photos');
+
+      item[0].style.visibility = 'hidden';
+
+      $timeout(500).then(() => item[0].style.visibility = 'visible');
+    }
+
+    function testScroll() {
+
+      let scrollingElem = document.getElementsByClassName('service-point-picture');
+
+      $document.ready(function () {
+        let elem = document.getElementsByClassName('picture-gallery');
+
+        if (elem[0].childNodes[1].scrollHeight > elem[0].childNodes[1].clientHeight) {
+          vm.scrollToGallery = true;
+        }
+
+        angular.element(scrollingElem).on('scroll', debounceFunc);
+
+      });
+
+      let debounceFunc = _.debounce(function ($event) {
+
+        if ($event.srcElement.scrollTop > 210) {
+          vm.scrollToGallery = false;
+        } else {
+          vm.scrollToGallery = true;
+        }
+
+      }, 100);
+
+    }
+
+    function onChangeFile(item) {
+
+      if (item) {
+        vm.uploadingPicture = true;
+        vm.pictureUpload(item);
+      }
+
+    }
+
+    function photoClick(photo) {
+
+      $scope.imagesAll = vm.servicePoint.pictures;
+      vm.thumbnailClick(photo);
+
+    }
+
+    function pictureUpload(item) {
+
+      if (!item) return;
+
+      let helper = new PictureHelper();
+
+      let picture = Picture.createInstance({
+        ownerXid: vm.servicePoint.id,
+        target: 'ServicePoint'
+      });
+
+      let busy = helper.onSelect(item, picture)
+        .then(() => {
+          vm.servicePoint.DSCreate().then(() => {
+            vm.tileBusy = {};
+            vm.progress = {};
+            vm.uploadingPicture = false;
+          })
+        })
+        .catch(err => {
+          console.error(err);
+        });
+
+      vm.progress = helper;
+      vm.tileBusy = {promise: busy, message: 'Nuotraukos siuntimas'};
+
+    }
 
     function phoneTo(ev, phone) {
       ev.stopPropagation();
@@ -63,7 +152,7 @@
       vm.rebindOne(ServicePoint, id, 'vm.servicePoint');
 
       let busy = [
-        ServicePoint.findAllWithRelations({id}, {bypassCache: true})(['ServiceItem', 'ServicePointContact'])
+        ServicePoint.findAllWithRelations({id}, {bypassCache: true})(['ServiceItem', 'ServicePointContact', 'Picture'])
           .then(loadServicePointRelations)
       ];
 
@@ -79,6 +168,8 @@
       _.each(servicePoint.servingItems, serviceItem => {
         serviceItem.DSLoadRelations();
       });
+
+      testScroll();
 
     }
 
