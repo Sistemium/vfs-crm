16d3fa2896fe796839c89fd0a6eee353ec429de8
Alexander Levin <alevin.ru@gmail.com>
2017-08-27 22:16:02 +0300

    editServicePoint with streets

--- src/app/domain/servicePoint/editServicePoint/editServicePoint.component.js
+++ src/app/domain/servicePoint/editServicePoint/editServicePoint.component.js
@@ -13,7 +13,37 @@
 
   });
 
-  function editServicePointController() {
+  function editServicePointController(saControllerHelper, $scope) {
+
+    const vm = saControllerHelper.setup(this, $scope);
+
+    vm.use({
+      $onInit
+    });
+
+    function $onInit() {
+
+      let {servicePoint} = vm;
+
+      servicePoint && servicePoint.DSLoadRelations('Street')
+        .then(() => servicePoint.street && servicePoint.street.DSLoadRelations())
+        .then(() => {
+          let locality = _.get(servicePoint, 'street.locality') || {};
+          vm.use({
+            districtId: locality.districtId,
+            localityId: locality.id
+          })
+        });
+
+      vm.watchScope('vm.servicePoint', onChange, true);
+
+    }
+
+    function onChange(servicePoint) {
+      if (!servicePoint) return;
+      servicePoint.address = servicePoint.concatAddress();
+    }
+
   }
 
 })(angular.module('webPage'));
