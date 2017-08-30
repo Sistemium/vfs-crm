cc060625c7bd6be58e746f753475355f2583e5c4
TaWReOn <deniss.mosin@gmail.com>
2017-08-30 15:17:47 +0300

    show map func

--- src/app/domain/servicePoint/details/servicePointDetails.component.js
+++ src/app/domain/servicePoint/details/servicePointDetails.component.js
@@ -8,7 +8,7 @@
 
   });
 
-  function servicePointDetailsController($scope, Schema, saControllerHelper, $state, Editing, PictureHelper, GalleryHelper, $document, NgMap, mapsHelper, GeoCoder) {
+  function servicePointDetailsController($scope, Schema, saControllerHelper, $state, Editing, PictureHelper, GalleryHelper, $document, NgMap, mapsHelper, GeoCoder, $uibModal, OnMap) {
 
     const vm = saControllerHelper.setup(this, $scope)
       .use(GalleryHelper);
@@ -29,6 +29,7 @@
       photoClick,
       testScroll,
       $onInit,
+      test,
 
       editPhoto: Editing.editModal('edit-picture', 'Fotografijos Redagavimas'),
       editServicePointClick: Editing.editModal('edit-service-point', 'Aptarnavimo Taško Redagavimas'),
@@ -47,7 +48,62 @@
      Functions
      */
 
+    function test(servicePoint, coords) {
+
+      let modalInstance = $uibModal.open({
+        animation: true,
+        template: `       
+         
+        <div class="modal-header" style="padding: 0 15px">
+          <h3 style="margin-top: 10px">{{vm.servicePoint.address}}</h3>
+        </div>
+        
+        <div class="modal-body" id="modal-body" resize resize-offset-top="190" resize-property="height">
+          <ng-map
+          ng-if="vm.isReady"
+          style="height: 70%"
+          center='[{{vm.coords.lat()}}, {{vm.coords.lng()}}]'
+          zoom="13"
+          >
+            <marker 
+              position="{{vm.coords.lat()}}, {{vm.coords.lng()}}"
+              animation="DROP"
+              draggable="true">      
+            </marker>
+          </ng-map>   
+        </div>`,
+
+        size: 'lg',
+
+        controller
+
+      });
+
+      modalInstance.result
+        .then(() => {
+        }, () => {
+        });
+
+      function controller($scope) {
+
+        const vm = {};
+
+        modalInstance.rendered
+          .then(() => {
+            vm.isReady = true;
+          });
+
+        $scope.vm = vm;
+
+        vm.servicePoint = servicePoint;
+        vm.coords = coords;
+
+      }
+    }
+
     function $onInit() {
+
+      vm.openMap = OnMap.open();
 
       // let item = document.getElementsByClassName('more-photos');
       //
