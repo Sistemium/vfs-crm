e97222b322a8f19f515500bab323bbff5570f412
Alexander Levin <alevin.ru@gmail.com>
2017-09-10 12:19:26 +0300

    formatting

--- src/app/components/bodyController.js
+++ src/app/components/bodyController.js
@@ -2,31 +2,31 @@
 
 (function () {
 
-    angular.module('webPage').controller('BodyController', function (appcache, toastr, $window) {
+  angular.module('webPage').controller('BodyController', function (appcache, toastr, $window) {
 
-      const vm = this;
-      const ua = new UAParser();
-      const deviceInfo = ua.getOS();
+    const vm = this;
+    const ua = new UAParser();
+    const deviceInfo = ua.getOS();
 
-      vm.cls = deviceInfo.name ? deviceInfo.name.replace (' ','') : '';
-      vm.cacheStatus = function () {
-        return appcache.textStatus;
-      };
+    vm.cls = deviceInfo.name ? deviceInfo.name.replace(' ', '') : '';
+    vm.cacheStatus = function () {
+      return appcache.textStatus;
+    };
 
-      function onUpdate () {
-        toastr.error ('Paspauskite, kad taikyti', 'Gautas atnaujinimas', {
-          timeOut: 0,
-          extendedTimeOut: 0,
-          onTap: function () {
-            $window.location.reload (true);
-          }
-        });
-      }
+    function onUpdate() {
+      toastr.error('Paspauskite, kad taikyti', 'Gautas atnaujinimas', {
+        timeOut: 0,
+        extendedTimeOut: 0,
+        onTap: function () {
+          $window.location.reload(true);
+        }
+      });
+    }
 
-      $window.stmAppCacheUpdated = onUpdate;
+    $window.stmAppCacheUpdated = onUpdate;
 
-      appcache.addEventListener('updateready', onUpdate, true);
+    appcache.addEventListener('updateready', onUpdate, true);
 
-    });
+  });
 
 })();
