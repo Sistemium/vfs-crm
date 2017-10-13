53d9a4d8fa263ed60690f1c4e8ae11eff443b591
TaWReOn <deniss.mosin@gmail.com>
2017-10-13 19:17:12 +0300

    editDistrict component ctrl added, bindings added

--- src/app/domain/addressing/editDistrict/editDistrict.component.js
+++ src/app/domain/addressing/editDistrict/editDistrict.component.js
@@ -3,13 +3,36 @@
   module.component('editDistrict', {
 
     bindings: {
-      district: '='
+      district: '=ngModel',
+      readyState: '='
     },
 
     templateUrl: 'app/domain/addressing/editDistrict/editDistrict.html',
+    controller: editDistrictController,
     controllerAs: 'vm'
 
   });
 
+  function editDistrictController($scope, UUID, saControllerHelper) {
+
+    const vm = saControllerHelper.setup(this, $scope);
+
+    vm.use({
+      id: `edit-district-${UUID.v4()}`
+    });
+
+    $scope.$watch(() => vm.district.isValid(), (nv) => {
+
+      if (!!nv) {
+        vm.readyState[vm.id] = true;
+      }
+
+    });
+
+    $scope.$on('$destroy', () => {
+      delete vm.readyState[vm.id]
+    });
+
+  }
 
 })(angular.module('webPage'));
