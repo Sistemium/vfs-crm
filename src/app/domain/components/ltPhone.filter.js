1d3b6688b9947c517bee3d002792f557c2d605ff
TaWReOn <deniss.mosin@gmail.com>
2017-11-17 18:34:05 +0200

    if condition

--- src/app/domain/components/ltPhone.filter.js
+++ src/app/domain/components/ltPhone.filter.js
@@ -3,15 +3,13 @@
   angular.module('core.services')
     .filter('ltphone', ltPhoneFilter);
 
-
   function ltPhoneFilter() {
 
     return function (input) {
 
-      if (!input) return '';
+      if (!input || input.length !== 8) return '';
 
       let parts = input.match(/(\d{3})(\d{2})(\d{3})/);
-
       return `+370 (${parts[1]}) ${parts[2]}-${parts[3]}`;
 
     };
