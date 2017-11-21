3b2af45ffcca32023627445603c6db40b4ecf8b2
TaWReOn <deniss.mosin@gmail.com>
2017-11-21 18:53:30 +0200

    Contact computed function

--- src/app/domain/models/base/Contact.model.js
+++ src/app/domain/models/base/Contact.model.js
@@ -15,9 +15,27 @@
             localKey: 'contactMethodId'
           }
         }
+      },
+
+      computed: {
+        href: ['address', 'contactMethodId', href]
       }
 
     });
+
+    function href(address, contactMethodId) {
+
+      if (!address) {
+        return;
+      }
+
+      const {ContactMethod} = Schema.models();
+
+      let {link} = ContactMethod.get(contactMethodId);
+
+      return link + address;
+
+    }
 
   });
 
