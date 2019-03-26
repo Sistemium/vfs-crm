4a00c828e4f099267c19daa0000e92c73fec109d
Alexander Levin <alevin.ru@gmail.com>
2019-03-26 16:43:50 +0200

    ServiceItemService with type-switch

--- src/app/domain/models/service/ServiceItemService.model.js
+++ src/app/domain/models/service/ServiceItemService.model.js
@@ -22,7 +22,11 @@
       },
 
       methods: {
-        isValid
+        isValid,
+      },
+
+      computed: {
+        typeIcon: ['type', typeIcon],
       },
 
       meta: {
@@ -30,12 +34,29 @@
           add: 'Naujas įrenginio aptarnavimas',
           about: 'įrenginio aptarnavimo'
         }
-      }
+      },
+
+      beforeCreateInstance: function (model, item) {
+
+        return _.defaults(item, { type: 'service' });
+
+      },
 
     });
 
     function isValid(readyState) {
       return this.date && (this.serviceItemId || _.get(readyState, 'serviceItemId.ready'));
+    }
+
+    function typeIcon(type) {
+      switch (type) {
+        case 'pause':
+          return 'red glyphicon glyphicon-pause';
+        case 'forward':
+          return 'blue glyphicon glyphicon-forward';
+        default:
+          return 'green glyphicon glyphicon-check';
+      }
     }
 
   });
