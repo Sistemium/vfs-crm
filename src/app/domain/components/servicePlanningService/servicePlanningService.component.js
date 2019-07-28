d9a2637a86fd6ef14687012a1871720d705afffb
Alexander Levin <alevin.ru@gmail.com>
2019-07-28 14:05:41 +0300

    Update servicePlanningService.component.js

--- src/app/domain/components/servicePlanningService/servicePlanningService.component.js
+++ src/app/domain/components/servicePlanningService/servicePlanningService.component.js
@@ -16,69 +16,72 @@
 
   function servicePlanningServiceController($scope, saControllerHelper, Schema, $q, moment) {
 
-    const vm = saControllerHelper.setup(this, $scope)
-      .use({
-        saveItemServiceClick,
-        newServiceDate: null,
-        nextServiceDate: null,
-        nextServiceInfo: '',
-        newServiceInfo: '',
-        type: 'service',
-        placeholder: {
-          service: {
-            a: 'Papildomas aptarnavimas',
-            b: 'Sekančiam aptarnavimui',
-          },
-          pause: {
-            a: 'Komentaras',
-            b: 'Sekančiam aptarnavimui',
-          },
-          forward: {
-            a: 'Priežastis',
-            b: 'Sekančiam aptarnavimui',
-          },
-        },
-      });
-
     const {
       ServiceItemService,
       ServicePlanning,
+      ServiceItem,
     } = Schema.models('');
 
+    saControllerHelper.setup(this, $scope).use({
+
+      newServiceDate: null,
+      nextServiceDate: null,
+      nextServiceInfo: '',
+      newServiceInfo: '',
+      type: 'service',
+
+      placeholder: {
+        service: {
+          a: 'Papildomas aptarnavimas',
+          b: 'Sekančiam aptarnavimui',
+        },
+        pause: {
+          a: 'Komentaras',
+          b: 'Sekančiam aptarnavimui',
+        },
+        forward: {
+          a: 'Priežastis',
+          b: 'Sekančiam aptarnavimui',
+        },
+      },
+
+      saveItemServiceClick() {
+
+        const { serviceItemId, servingMasterId, id, serviceItem } = this.servicePlanning;
+        const { newServiceInfo, nextServiceInfo, newServiceDate, type } = this;
+
+        let date = newServiceDate;
+
+        const promises = [];
+
+        if (type === 'pause') {
+          serviceItem.pausedFrom = newServiceDate;
+          promises.push(serviceItem.DSCreate());
+        } else if (type === 'forward') {
+          date = moment(this.monthDate).format('YYYY-MM-DD');
+          serviceItem.nextServiceDate = newServiceDate;
+          promises.push(serviceItem.DSCreate());
+        }
+
+        $q.all(promises)
+          .then(() => ServiceItemService.create({
+            serviceItemId,
+            servingMasterId,
+            nextServiceInfo,
+            type,
+            date,
+            info: newServiceInfo,
+          }))
+          .then(() => ServicePlanning.find(id, { bypassCache: true }))
+          .then(() => ServiceItem.find(serviceItemId, { bypassCache: true }));
+
+      },
+
+    });
 
     /*
     Functions
      */
-
-    function saveItemServiceClick() {
-
-      const { serviceItemId, servingMasterId, id, serviceItem } = vm.servicePlanning;
-      const { newServiceInfo, nextServiceInfo, newServiceDate, type } = vm;
-
-      let date = newServiceDate;
-
-      const promises = [];
-
-      if (type === 'pause') {
-        serviceItem.pausedFrom = newServiceDate;
-        promises.push(serviceItem.DSCreate());
-      } else if (type === 'forward') {
-        date = moment(vm.monthDate).format('YYYY-MM-DD');
-        serviceItem.nextServiceDate = newServiceDate;
-        promises.push(serviceItem.DSCreate());
-      }
-
-      $q.all(promises)
-        .then(() => ServiceItemService.create({
-          serviceItemId,
-          servingMasterId,
-          nextServiceInfo,
-          type,
-          date,
-          info: newServiceInfo,
-        }))
-        .then(() => ServicePlanning.find(id, { bypassCache: true }));
-    }
 
   }
 
