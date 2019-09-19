4ca04b1e40b1a924f2fd823199fcba915411d431
Alexander Levin <alevin.ru@gmail.com>
2019-09-19 11:47:06 +0300

    editServiceContract legalType fix

--- src/app/domain/serviceContract/editServiceContract/editServiceContract.component.js
+++ src/app/domain/serviceContract/editServiceContract/editServiceContract.component.js
@@ -15,49 +15,60 @@
 
   function editServiceContract($scope, ReadyStateHelper, Schema, $state) {
 
-    const vm = ReadyStateHelper.setupController(this, $scope, 'serviceContract');
+    const config = {
 
-    vm.use({
-      $onInit,
-      servicePointClick
-    });
+      $onInit() {
 
-    /*
-     Functions
-     */
-
-    function $onInit() {
-
-      const {serviceContract} = vm;
-      const {Brand, FilterSystemType, FilterSystem, Site} = Schema.models();
+        const { serviceContract } = this;
+        const { Brand, FilterSystemType, FilterSystem, Site } = Schema.models();
 
 
-      if (!serviceContract.id && serviceContract.hasOwnProperty('siteId')) {
-        vm.hideSite = true;
-      } else if (serviceContract && !serviceContract.id) {
-        serviceContract.siteId = _.get(Site.meta.getCurrent(), 'id');
-        vm.hideSite = true;
-      }
+        if (!serviceContract.id && serviceContract.hasOwnProperty('siteId')) {
+          this.hideSite = true;
+        } else if (serviceContract && !serviceContract.id) {
+          serviceContract.siteId = _.get(Site.meta.getCurrent(), 'id');
+          this.hideSite = true;
+        }
 
-      vm.legalType = serviceContract.legalType;
+        serviceContract.DSCompute();
+        this.legalType = serviceContract.legalType;
 
-      vm.serviceContract.DSLoadRelations()
-        .then(serviceContract => {
-          return _.map(serviceContract.servicePoints, servicePoint => {
-            return servicePoint.DSLoadRelations();
-          });
-        })
-        .then(() => {
-          Brand.findAll();
-          FilterSystemType.findAll();
-          FilterSystem.findAll();
+        const types = [
+          'vm.legalType',
+          'vm.serviceContract.customerPersonId',
+          'vm.serviceContract.customerLegalEntityId'
+        ];
+
+        $scope.$watchGroup(types, ([legalType, personId, legalEntityId]) => {
+          if (legalType === 'Asmuo' && personId) {
+            this.serviceContract.customerLegalEntityId = null;
+          } else if (legalType === 'Įmonė' && legalEntityId) {
+            this.serviceContract.customerPersonId = null;
+          }
         });
 
-    }
+        this.serviceContract.DSLoadRelations()
+          .then(serviceContract => {
+            return _.map(serviceContract.servicePoints, servicePoint => {
+              return servicePoint.DSLoadRelations();
+            });
+          })
+          .then(() => {
+            Brand.findAll();
+            FilterSystemType.findAll();
+            FilterSystem.findAll();
+          });
 
-    function servicePointClick(item) {
-      $state.go('servicePoint.detailed', {servicePointId: item.id}, {reload: true});
-    }
+      },
+
+      servicePointClick(item) {
+        $state.go('servicePoint.detailed', { servicePointId: item.id }, { reload: true });
+      }
+
+    };
+
+    ReadyStateHelper.setupController(this, $scope, 'serviceContract')
+      .use(config)
 
   }
 
