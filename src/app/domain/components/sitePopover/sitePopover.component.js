2efbbe50613bea9764cfd93ba72a9472c29f804d
Alexander Levin <alevin.ru@gmail.com>
2020-10-07 13:40:39 +0300

    sitePopover style

--- src/app/domain/components/sitePopover/sitePopover.component.js
+++ src/app/domain/components/sitePopover/sitePopover.component.js
@@ -15,18 +15,22 @@
 
   function sitePopoverController($scope, Schema, saControllerHelper) {
 
-    const {Site} = Schema.models();
+    const { Site } = Schema.models();
 
     const vm = saControllerHelper.setup(this, $scope)
       .use({
-        optionClick
+        popoverIsOpen: false,
+        optionClick,
       });
 
     vm.rebindAll(Site, {}, 'vm.data', initCurrent);
 
     function initCurrent() {
       Site.meta.initCurrent()
-        .then(modelCurrent => vm.current = modelCurrent);
+        .then(modelCurrent => {
+          vm.current = modelCurrent;
+          vm.popoverIsOpen = !modelCurrent;
+        });
     }
 
     function optionClick(option) {
